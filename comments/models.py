24fd21ed0a734c9410731e4d3c15fb646edc0f9f
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-08-16 20:25:08 +0300

    fix models.py for work with files and media

--- comments/models.py
+++ comments/models.py
@@ -1,11 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import User
+
+from user.models import User
+
+from PIL import Image
 
 
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="comments/images/", null=True, blank=True)
+    text_file = models.FileField(upload_to="comments/texts/", null=True, blank=True)
 
-    def __str__(self):
-        return self.text[:20]
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.image:
+            img = Image.open(self.image)
+            img.thumbnail((320, 240))
+            img.save(self.image.path)
