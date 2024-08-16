d0aadb427a2dce4e39832a0ad799550bee6ef957
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-08-16 20:22:16 +0300

    fix asgi.py for dcrf

--- test_chat/asgi.py
+++ test_chat/asgi.py
@@ -3,19 +3,15 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
-from comments.urls import websocket_urlpatterns
+from comments.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_chat.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_chat.settings")
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                websocket_urlpatterns
-            )
-        )
-    ),
-})
-
-
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
