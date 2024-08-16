77af98a4357b49d939f4d35e2d69a832a0fe8dcf
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-08-16 20:29:29 +0300

    update endpoints

--- test_chat/urls.py
+++ test_chat/urls.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from comments.views import CommentViewSet
 
-router = DefaultRouter()
-router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('', include('comments.urls')),
+    path("admin/", admin.site.urls),
+    path("api/user/", include("user.urls"), name="user"),
+    path("", include("comments.urls"), name="comments"),
 ]
