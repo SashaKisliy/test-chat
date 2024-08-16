2d67b1820ea5bb5ccc16a71ffca27081d4ce6ca2
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-08-16 20:26:17 +0300

    update serializers.py for work with files and media

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,8 +1,20 @@
+import bleach
 from rest_framework import serializers
 from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(required=False)
+    file = serializers.FileField(required=False)
+
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = ["author", "text", "created_at", "image", "file"]
+
+    def create(self, validated_data):
+        if "user" in validated_data:
+            validated_data["author"] = validated_data.pop("user")
+        validated_data["text"] = bleach.clean(
+            validated_data["text"], tags=["a", "code", "i", "strong"]
+        )
+        return super().create(validated_data)
